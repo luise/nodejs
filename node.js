754d0fb0cf8a94219dd81c237e279fc1ff40fd82
Kevin Lin <kaikai526@gmail.com>
2017-02-19 17:29:28 -0600

    node: Remove unnecessary `connect` and environment variable
    The example Docker image wasn't using them.

--- node.js
+++ node.js
@@ -12,13 +12,6 @@ function Node(cfg) {
   var env = cfg.env || {};
   var containers = new Container(cfg.image).withEnv(env).replicate(cfg.nWorker);
   this._app = new Service("app", containers);
-
-  var hosts = this._app.children();
-  for (var i = 0; i < containers.length; i++) {
-    containers[i].setEnv("HOST", hosts[i]);
-  }
-
-  this._app.connect(this._port, this._app);
 };
 
 Node.prototype.deploy = function(deployment) {
